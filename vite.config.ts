26150204bd25942994bb9303353895cd512aa8bf
serverfas-dot <server.sammu@gmail.com>
2026-05-05 15:07:25 +0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_ACTIONS ? '/eid-form/' : '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
